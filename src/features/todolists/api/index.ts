8b68d888d0a34a55944ed2cd6ab8e8685cd38992
nedonebo21 <cursed.void10007@gmail.com>
2025-07-12 17:10:39 +0400

    refactor imports index.ts

---
+++ src/features/todolists/api/index.ts
@@ -0,0 +1,4 @@
+export {todolistsApi} from "./todolists-api.ts"
+export {tasksApi} from "./tasks-api.ts"
+export type {Todolist} from './todolists-api.types.ts'
+export type {DomainTask, GetTasksResponse, UpdateTaskModel} from './tasks-api.types.ts'
