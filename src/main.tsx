b6fb14511a7e940cd31786195be0a42e6a379951
nedonebo21 <cursed.void10007@gmail.com>
2025-07-18 12:58:00 +0400

    swap app-http-req to App

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { createRoot } from "react-dom/client"
 import "./shared/styles/index.css"
-// import { App } from "./app/App.tsx"
+import { App } from "./app/App.tsx"
 import { Providers } from "@/app/providers"
-import {AppWithHttpRequest} from "@/app/app-with-http-request.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <Providers>
-    {/*<App />*/}
-    <AppWithHttpRequest/>
+    <App/>
   </Providers>,
 )
