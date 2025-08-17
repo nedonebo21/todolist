d671a76f3455ffc25fb14b9876934cb352ff48e3
nedonebo21 <cursed.void10007@gmail.com>
2025-08-17 13:28:30 +0400

    dispatch initializeApp in useEffect

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,19 @@
 import { Header } from '@/components/header/header.tsx'
 import { DomainTask } from '@/features/todolists/api'
 import { Routing } from '@/shared/routing'
+import { useAppDispatch } from '@/shared/lib/hooks'
+import { useEffect } from 'react'
+import { initializeAppTC } from '@/features/auth/model/auth-slice.ts'
 
 export type TasksState = Record<string, DomainTask[]>
 
 export type FilterValues = 'all' | 'active' | 'completed'
 
 export const App = () => {
+   const dispatch = useAppDispatch()
+   useEffect(() => {
+      dispatch(initializeAppTC())
+   }, [])
    return (
       <div className="app">
          <Header />
