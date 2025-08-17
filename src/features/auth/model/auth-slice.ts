21698e4863c3810360d5fdf09df9721a91d697a8
nedonebo21 <cursed.void10007@gmail.com>
2025-08-17 12:31:39 +0400

    save token to LC

--- src/features/auth/model/auth-slice.ts
+++ src/features/auth/model/auth-slice.ts
@@ -2,6 +2,8 @@ import { createAppSlice, handleServerAppError, handleServerNetworkError } from '
 import { LoginInputs } from '@/features/auth/lib/schemas/login-schema.ts'
 import { setAppStatusAC } from '@/app/app-slice.ts'
 import { authApi } from '@/features/auth/api/auth-api.ts'
+import { ResultCode } from '@/shared/enums/enums.ts'
+import { AUTH_TOKEN } from '@/shared/constants'
 
 export const authSlice = createAppSlice({
    name: 'auth',
@@ -17,8 +19,9 @@ export const authSlice = createAppSlice({
             try {
                dispatch(setAppStatusAC({ status: 'pending' }))
                const res = await authApi.login(data)
-               if (res.data.resultCode === 0) {
+               if (res.data.resultCode === ResultCode.Success) {
                   dispatch(setAppStatusAC({ status: 'succeeded' }))
+                  localStorage.setItem(AUTH_TOKEN, res.data.data.token)
                   return { isLoggedIn: true }
                } else {
                   handleServerAppError(res.data, dispatch)
