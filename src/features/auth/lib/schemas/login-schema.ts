601e2f17fadb0da33ed055129bfbf04f676e95de
nedonebo21 <cursed.void10007@gmail.com>
2025-08-17 12:08:25 +0400

    add captcha to login-schema

--- src/features/auth/lib/schemas/login-schema.ts
+++ src/features/auth/lib/schemas/login-schema.ts
@@ -1,12 +1,13 @@
 import { z } from 'zod/v4'
 
 export const loginSchema = z.object({
-   email: z.email(),
+   email: z.email({ error: 'Incorrect email address' }),
    password: z
       .string()
       .min(1, 'Password is required')
       .min(3, 'Password must be at least 3 characters long'),
    rememberMe: z.boolean(),
+   captcha: z.string().optional(),
 })
 
 export type LoginInputs = z.infer<typeof loginSchema>
