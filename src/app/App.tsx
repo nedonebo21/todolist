5085a237db21f97f5877131d8ca24b398a54fb17
nedonebo21 <cursed.void10007@gmail.com>
2025-08-17 14:18:28 +0400

    fix redirect bug

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,26 @@ import { Header } from '@/components/header/header.tsx'
 import { DomainTask } from '@/features/todolists/api'
 import { Routing } from '@/shared/routing'
 import { useAppDispatch } from '@/shared/lib/hooks'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { initializeAppTC } from '@/features/auth/model/auth-slice.ts'
+import { Progress } from '@/shared/ui/shadcn/progress.tsx'
 
 export type TasksState = Record<string, DomainTask[]>
 
 export type FilterValues = 'all' | 'active' | 'completed'
 
 export const App = () => {
+   const [isInitialized, setIsInitialized] = useState(false)
    const dispatch = useAppDispatch()
+
    useEffect(() => {
-      dispatch(initializeAppTC())
+      dispatch(initializeAppTC()).finally(() => {
+         setIsInitialized(true)
+      })
    }, [])
+   if (!isInitialized) {
+      return <Progress />
+   }
    return (
       <div className="app">
          <Header />
