8d76dc9cd392e4915f74e6c4c8d806f549a0ab5b
nedonebo21 <cursed.void10007@gmail.com>
2025-08-17 13:30:02 +0400

    refactor routing comp

--- src/shared/routing/routing.tsx
+++ src/shared/routing/routing.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router'
 import { Main } from '@/app/main.tsx'
 import { PageNotFound } from '@/shared/ui/page-not-found/page-not-found.tsx'
 import { Login } from '@/features/auth'
+import { useAppSelector } from '@/shared/lib/hooks'
+import { selectIsLoggedIn } from '@/features/auth/model/auth-slice.ts'
+import { ProtectedRoute } from './protected-route'
 
 export const Path = {
    Main: '/',
@@ -10,10 +13,15 @@ export const Path = {
 }
 
 export const Routing = () => {
+   const isLoggedIn = useAppSelector(selectIsLoggedIn)
    return (
       <Routes>
-         <Route path={Path.Main} element={<Main />} />
-         <Route path={Path.Login} element={<Login />} />
+         <Route element={<ProtectedRoute isAllowed={isLoggedIn} />}>
+            <Route path={Path.Main} element={<Main />} />
+         </Route>
+         <Route element={<ProtectedRoute isAllowed={!isLoggedIn} redirectPath={Path.Main} />}>
+            <Route path={Path.Login} element={<Login />} />
+         </Route>
          <Route path={Path.NotFound} element={<PageNotFound />} />
       </Routes>
    )
