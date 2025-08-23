6bb40708efef68e5733f0cee9b41219598d3bfae
nedonebo21 <cursed.void10007@gmail.com>
2025-08-23 13:08:33 +0400

    refactor updateTask on RTK Query

--- src/features/todolists/api/tasks-api.ts
+++ src/features/todolists/api/tasks-api.ts
@@ -1,30 +1,6 @@
-import { instance } from '@/shared/api'
 import { DomainTask, GetTasksResponse, UpdateTaskModel } from '@/features/todolists/api'
 import { BaseResponse } from '@/shared/types'
 import { baseApi } from '@/app/base-api.ts'
-
-export const _tasksApi = {
-   getTasks(todolistId: string) {
-      return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
-   },
-   addTask(payload: { todolistId: string; title: string }) {
-      const { todolistId, title } = payload
-      return instance.post<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks`, {
-         title,
-      })
-   },
-   deleteTask(payload: { todolistId: string; taskId: string }) {
-      const { todolistId, taskId } = payload
-      return instance.delete<BaseResponse>(`/todo-lists/${todolistId}/tasks/${taskId}`)
-   },
-   updateTask(payload: { todolistId: string; taskId: string; model: UpdateTaskModel }) {
-      const { todolistId, taskId, model } = payload
-      return instance.put<BaseResponse<{ item: DomainTask }>>(
-         `/todo-lists/${todolistId}/tasks/${taskId}`,
-         model
-      )
-   },
-}
 
 export const tasksApi = baseApi.injectEndpoints({
    endpoints: build => ({
@@ -50,7 +26,23 @@ export const tasksApi = baseApi.injectEndpoints({
          }),
          invalidatesTags: ['Task'],
       }),
+      updateTask: build.mutation<
+         BaseResponse,
+         { todolistId: string; taskId: string; model: UpdateTaskModel }
+      >({
+         query: ({ todolistId, taskId, model }) => ({
+            url: `todo-lists/${todolistId}/tasks/${taskId}`,
+            method: 'PUT',
+            body: model,
+         }),
+         invalidatesTags: ['Task'],
+      }),
    }),
 })
 
-export const { useGetTasksQuery, useAddTaskMutation, useDeleteTaskMutation } = tasksApi
+export const {
+   useGetTasksQuery,
+   useAddTaskMutation,
+   useDeleteTaskMutation,
+   useUpdateTaskMutation,
+} = tasksApi
