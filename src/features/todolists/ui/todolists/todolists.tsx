4a44090d4f5eb202e69fca81ef652eb371a0df31
nedonebo21 <cursed.void10007@gmail.com>
2025-08-23 11:01:16 +0400

    add skip arg to useQuery

--- src/features/todolists/ui/todolists/todolists.tsx
+++ src/features/todolists/ui/todolists/todolists.tsx
@@ -2,7 +2,7 @@ import { TodolistItem } from '@/features/todolists/ui/todolists/todolist-item/to
 import { useGetTodolistsQuery } from '@/features/todolists/api/todolists-api.ts'
 
 export const Todolists = () => {
-   const { data: todoLists } = useGetTodolistsQuery()
+   const { data: todoLists } = useGetTodolistsQuery(undefined, { skip: true })
 
    return todoLists?.map(todolist => {
       return <TodolistItem todolist={todolist} key={todolist.id} />
