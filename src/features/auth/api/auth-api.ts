e7c4fcd4f067ab537be8f83aeaa0657eaafda260
nedonebo21 <cursed.void10007@gmail.com>
2025-08-23 12:00:10 +0400

    refactor authApi on RTK Query, delete auth-slice

--- src/features/auth/api/auth-api.ts
+++ src/features/auth/api/auth-api.ts
@@ -1,15 +1,25 @@
 import { LoginInputs } from '@/features/auth/lib/schemas/login-schema.ts'
-import { instance } from '@/shared/api'
 import { BaseResponse } from '@/shared/types'
+import { baseApi } from '@/app/base-api.ts'
 
-export const authApi = {
-   login(payload: LoginInputs) {
-      return instance.post<BaseResponse<{ userId: number; token: string }>>('/auth/login', payload)
-   },
-   logout() {
-      return instance.delete<BaseResponse>('/auth/login')
-   },
-   me() {
-      return instance.get<BaseResponse<{ id: number; email: string; login: string }>>('/auth/me')
-   },
-}
+export const authApi = baseApi.injectEndpoints({
+   endpoints: build => ({
+      me: build.query<BaseResponse<{ id: string; email: string; login: string }>, void>({
+         query: () => 'auth/me',
+      }),
+      login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginInputs>({
+         query: body => ({
+            url: 'auth/login',
+            method: 'POST',
+            body,
+         }),
+      }),
+      logout: build.mutation<BaseResponse, void>({
+         query: () => ({
+            url: 'auth/login',
+            method: 'DELETE',
+         }),
+      }),
+   }),
+})
+export const { useMeQuery, useLoginMutation, useLogoutMutation } = authApi
