833840871882b224b49a416247e6621b8418535f
nedonebo21 <cursed.void10007@gmail.com>
2025-08-30 12:44:44 +0400

    remove todolist/tasks reducers from store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { tasksReducer, tasksSlice } from '@/features/todolists/model/tasks-slice.ts'
-import { todolistReducer, todolistsSlice } from '@/features/todolists/model/todolists-slice.ts'
 import { appReducer, appSlice } from '@/app/app-slice.ts'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { baseApi } from '@/app/base-api.ts'
 
 export const store = configureStore({
    reducer: {
-      [tasksSlice.name]: tasksReducer,
-      [todolistsSlice.name]: todolistReducer,
       [appSlice.name]: appReducer,
       [baseApi.reducerPath]: baseApi.reducer,
    },
